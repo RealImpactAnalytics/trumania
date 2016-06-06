4cf7d2e9c92d26f48184a72c2e0368a84b6d4eaa
Gautier Krings <gautier.krings@realimpactanalytics.com>
2016-06-06 17:10:16 +0200

    created first tentative version of mobility

--- src/actor.py
+++ src/actor.py
@@ -16,6 +16,7 @@ class Actor(object):
         """
         IDs = np.arange(id_start, id_start + size)
         self._table = pd.DataFrame({"ID": IDs, "clock": 0})
+        self._transient_attributes = {}
 
     def add_attribute(self, name, generator):
         """Adds a column named "name" to the inner table of the actor, randomly generated from the generator.
@@ -69,7 +70,10 @@ class Actor(object):
         return self._table
 
 
-class CustomerActor(Actor):
+class CallerActor(Actor):
+    """
+    A CallerActor is an actor that makes calls
+    """
     def __init__(self, size, id_start=0):
         """
 
@@ -79,7 +83,7 @@ class CustomerActor(Actor):
         """
         Actor.__init__(self, size, id_start)
 
-    def make_calls(self, network, chooser, new_time_generator):
+    def make_actions(self, network, chooser, new_time_generator):
         """
 
         :param network:
@@ -94,4 +98,50 @@ class CustomerActor(Actor):
             self._table.loc[act_now.index, "clock"] = new_time_generator.generate(len(act_now.index))
             # TODO next generate time with different generators depending on customers
         self.update_clock()
+        return out
+
+
+class TransientAttribute(object):
+    """
+
+    """
+    def __init__(self, ids):
+        """
+
+        :param ids:
+        :return:
+        """
+        self._table = pd.DataFrame({"ID": ids, "value": np.NaN, "clock": 0})
+
+    def update(self, ids_to_update, values):
+        """
+
+        :param values:
+        :param ids_to_update:
+        :return:
+        """
+        self._table[self._table["ID"].isin(ids_to_update)]["value"] = values
+
+    def who_acts_now(self):
+        """
+
+        :return:
+        """
+        return self._table[self._table["clock"] == 0]
+
+    def make_actions(self, new_time_generator, relationship, id1, id2):
+        """
+
+        :param new_time_generator:
+        :param relationship:
+        :param id1:
+        :param id2:
+        :return:
+        """
+        act_now = self.who_acts_now()
+        out = pd.DataFrame(columns=["ID", "new"])
+        if len(act_now.index) > 0:
+            out = relationship.select_one(id1,act_now["ID"].values).rename(columns={id1:"ID",id2:"new"})
+            self._table[self._table["ID"].isin(out[id1].values)]["value"] = out["new"]
+            self._table[self._table["ID"].isin(out[id1].values)]["clock"] = new_time_generator.generate(len(act_now.index))
         return out
--- src/relationship.py
+++ src/relationship.py
@@ -10,9 +10,12 @@ class Relationship(object):
     def __init__(self, r1, r2, chooser):
         """
 
-        :param r1: string, name for first element
-        :param r2: string, name for second element
-        :param chooser:
+        :type r1: str
+        :param r1: name for first element
+        :type r2: str
+        :param r2: name for second element
+        :type chooser: random_generator.Chooser
+        :param chooser: Chooser object that will define the random selection of an element in the relationship
         :return:
         """
         cols = {r1: pd.Series(dtype=int),
@@ -23,11 +26,15 @@ class Relationship(object):
     def add_relation(self, r1, A, r2, B):
         """
 
-        :param r1:
-        :param A:
-        :param r2:
-        :param B:
-        :return:
+        :type r1: str
+        :param r1: name of the first column
+        :type A: Pandas Series object
+        :param A: all entries of the first column in the relationship
+        :type r2: str
+        :param r2: name of the second column
+        :type B: Pandas Series object
+        :param B: all entries of the second column in the relationship
+        :return: None
         """
         df = pd.DataFrame({r1: A, r2: B})
 
@@ -90,4 +97,87 @@ class WeightedRelationship(object):
         elif key_column == self.__r2:
             self.__chooser.update_choose_col(self.__r1)
         small_tab = self._table[self._table[key_column].isin(keys)]
+        return small_tab.groupby(key_column).aggregate(self.__chooser.generate).reset_index()
+
+
+class SimpleMobilityRelationship(WeightedRelationship):
+    """
+
+    """
+
+    def choose(self, clock, key_column, keys):
+        return self.select_one(key_column, keys)
+
+
+class HWRMobilityRelationship(WeightedRelationship):
+    """
+
+    """
+
+    def __init__(self,r1,r2,chooser,time_f):
+        """
+
+        :param r1:
+        :param r2:
+        :param chooser:
+        :param time_f:
+        :return:
+        """
+        cols = {r1: pd.Series(dtype=int),
+                r2: pd.Series(dtype=int),
+                "weight": pd.Series(dtype=float)}
+        self._home_table = pd.DataFrame(cols)
+        self._work_table = pd.DataFrame(cols)
+        self._random_table = pd.DataFrame(cols)
+        self.__chooser = chooser
+        self.__r1 = r1
+        self.__r2 = r2
+        self.__time_f = time_f
+
+    def add_home(self, r1, A, r2, B):
+        """
+
+        :param r1:
+        :param A:
+        :param r2:
+        :param B:
+        :return:
+        """
+        df = pd.DataFrame({r1: A, r2: B})
+        self._home_table = self._home_table.append(df, ignore_index=True)
+
+    def add_work(self, r1, A, r2, B):
+        """
+
+        :param r1:
+        :param A:
+        :param r2:
+        :param B:
+        :return:
+        """
+        df = pd.DataFrame({r1: A, r2: B})
+        self._work_table = self._work_table.append(df, ignore_index=True)
+
+    def add_random(self, r1, A, r2, B):
+        """
+
+        :param r1:
+        :param A:
+        :param r2:
+        :param B:
+        :return:
+        """
+        df = pd.DataFrame({r1: A, r2: B})
+        self._random_table = self._random_table.append(df, ignore_index=True)
+
+    def choose(self, clock, key_column, keys):
+        w_home,w_work,w_random = self.__time_f(clock)
+        small_home = self._home_table[self._home_table[self.__r1].isin(keys)].copy()
+        small_home["weight"] = small_home["weight"]*w_home
+        small_work = self._work_table[self._work_table[self.__r1].isin(keys)].copy()
+        small_work["weight"] = small_work["weight"]*w_work
+        small_random = self._random_table[self._random_table[self.__r1].isin(keys)].copy()
+        small_random["weight"] = small_random["weight"]*w_random
+
+        small_tab = pd.concat([small_home,small_work,small_random],ignore_index=True)
         return small_tab.groupby(key_column).aggregate(self.__chooser.generate).reset_index()
