583363469ccbde07e34356c31e501d24385844f4
Svend Vanderveken <svend.vanderveken@gmail.com>
2016-08-05 14:24:09 +0200

    trying to fix flags to run tests

--- run_tests_in_docker.sh
+++ run_tests_in_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 
-docker build -it 10.4.103.15:5000/lab_data-generator_tester$BUILD_NUMBER
-docker run 10.4.103.15:5000/lab_data-generator_tester$BUILD_NUMBER
+docker build -t 10.4.103.15:5000/lab_data-generator_tester$BUILD_NUMBER
+docker run -i 10.4.103.15:5000/lab_data-generator_tester$BUILD_NUMBER
 
