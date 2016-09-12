70c897c04914b9ca8114332d1336630b135ccc0c
Svend Vanderveken <svend.vanderveken@gmail.com>
2016-09-12 17:52:39 +0200

    cleaned up some commented out code

--- datagenerator/core/clock.py
+++ datagenerator/core/clock.py
@@ -33,13 +33,6 @@ class Clock(object):
         self.current_date = start
         self.step_s = step_s
         self.step_delta = timedelta(seconds=step_s)
-        # self.number_of_ticks_per_day = int(24 * 60 * 60 / step_s)
-        # self.number_of_ticks_per_week = 7 * self.number_of_ticks_per_day
-
-        # self.day_index = int((start.hour * 3600 + start.minute * 60
-        #                       + start.second) / step_s)
-        # self.week_index = int((start.weekday() * 24 * 3600) /
-        #                       step_s) + self.day_index
 
         self.output_format = output_format
         self.__state = RandomState(seed)
@@ -59,8 +52,6 @@ class Clock(object):
         :return: None
         """
         self.current_date += self.step_delta
-        # self.day_index = (self.day_index + 1) % self.number_of_ticks_per_day
-        # self.week_index = (self.week_index + 1) % self.number_of_ticks_per_week
 
         for listener in self.__increment_listeners:
             listener.increment()
@@ -78,24 +69,6 @@ class Clock(object):
             return (self.current_date + timedelta(seconds=x)).strftime(self.output_format)
 
         return pd.Series(self.__state.choice(self.step_s, size)).apply(make_ts)
-
-    # def get_week_index(self):
-    #     """Return the number of steps in which the Clock is from the start of the week.
-    #     For example, if the clock has a step of 3600s, Monday 12:00:00 returns 12, Tuesday 23:00:00 returns 47, etc...
-    #
-    #     :rtype: int
-    #     :return: the count of number of steps already taken since the start of the week
-    #     """
-    #     return self.week_index
-    #
-    # def get_day_index(self):
-    #     """Return the number of steps in which the Clock is from the start of the day.
-    #     For example, if the clock has a step of 3600s, 12:00:00 returns 12,  23:00:00 returns 23, etc...
-    #
-    #     :rtype: int
-    #     :return: the count of number of steps already taken since the start of the day
-    #     """
-    #     return self.day_index
 
     class ClockOps(object):
         def __init__(self, clock):
@@ -218,90 +191,3 @@ class CyclicTimerGenerator(DependentGenerator):
         p = draw / observations.values
         return pd.Series(self.profile["cdf"].searchsorted(p),
                          index=observations.index)
-
-
-# class TimerGenerator(DependentGenerator):
-#     """A TimeProfiler contains an activity profile over a defined time range.
-#     It's mostly a super class, normally only its child classes should be used.
-#
-#     The goal of a TimeProfiler is to keep a track of the expected level of activity of users over a cyclic time range
-#     It will store a vector with probabilities of activity per time step, as well as a cumulative sum of the probabilities
-#     starting with the current time step.
-#
-#     This allows to quickly produce random waiting times until the next event for the users
-#
-#     """
-#     def __init__(self, clock, profile, seed=None):
-#         """
-#         This should not be used, only child classes
-#
-#         :type clock: Clock
-#         :param clock: the master clock driving this simulator
-#         :type profile: Pandas Series, index is a timedelta object (minimum 1 sec and last is defined by the type of profile)
-#         values are floats
-#         :param profile: Weight of each period. The index indicate the right bound of the bin (left bound is 0 for the
-#         first bin and the previous index for all others)
-#         :type seed: int
-#         :param seed: seed for random number generator, default None
-#         :return: A new TimeProfiler is created
-#         """
-#         DependentGenerator.__init__(self)
-#         self._state = RandomState(seed)
-#
-#         totalweight = profile.sum()
-#         rightbounds = profile.index.values
-#
-#         leftbounds = np.append(np.array([np.timedelta64(-1, "s")]), profile.index.values[:-1])
-#         widths = rightbounds - leftbounds
-#         n_subbins = widths / np.timedelta64(clock.step_s, "s")
-#
-#         # normalized profile, with the time axis scaled up s.t. there is one
-#         # profile value for each clock step
-#         norm_prof = list(itertools.chain(
-#             *[n_subbins[i] * [profile.iloc[i] / float(n_subbins[i] * totalweight), ]
-#               for i in range(len(n_subbins))]))
-#
-#         self._profile = pd.DataFrame({"weight": norm_prof,
-#                                       "next_prob": np.nan,
-#
-#                                       #
-#                                       "timeframe": np.arange(len(norm_prof))})
-#
-#         # makes sure we'll get notified when the clock goes forward
-#         clock.register_increment_listener(self)
-#
-#     def increment(self):
-#         """
-#         Increment the time generator by 1 step.
-#
-#         This has as effect to move the cdf of one step to the right, decrease
-#         all values by the value of the original first entry, and placing the
-#         previous first entry at the end of the cdf, with value 1.
-#         """
-#
-#         # old_end_prob = self._profile.iloc[-1]["next_prob"]
-#         old_end_prob = self._profile["next_prob"].iloc[len(self._profile.index) - 1]
-#
-#         self._profile["next_prob"] -= self._profile["next_prob"].iloc[0]
-#
-#         self._profile = pd.concat([self._profile.iloc[1:len(self._profile.index)],
-#                                    self._profile.iloc[0:1]],
-#                                   ignore_index=True)
-#         self._profile.loc[self._profile.index[-1], "next_prob"] = old_end_prob
-#
-#     def generate(self, observations):
-#         """Generate random waiting times, based on some observed activity
-#         levels. The higher the level of activity, the shorter the waiting
-#         times will be
-#
-#         :type observations: Pandas Series
-#         :param observations: contains an array of floats
-#         :return: Pandas Series
-#         """
-#         if sum(np.isnan(self._profile["next_prob"])) > 0:
-#             raise Exception("Time profiler is not initialised!")
-#
-#         # uniform 0, 1
-#         p = self._state.rand(len(observations.index)) / observations.values
-#         return pd.Series(self._profile["next_prob"].searchsorted(p),
-#                          index=observations.index)
