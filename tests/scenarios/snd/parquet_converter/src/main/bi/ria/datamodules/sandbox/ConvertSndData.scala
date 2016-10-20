8f1b993f4424739f7b6afd00a085e175a07cfe45
Svend Vanderveken <svend.vanderveken@gmail.com>
2016-10-20 12:40:19 +0200

    adapted the scala converter for new POS attributes

--- tests/scenarios/snd/parquet_converter/src/main/bi/ria/datamodules/sandbox/ConvertSndData.scala
+++ tests/scenarios/snd/parquet_converter/src/main/bi/ria/datamodules/sandbox/ConvertSndData.scala
@@ -1,10 +1,9 @@
 package bi.ria.datamodules.sandbox
 
-import org.apache.spark.sql.SaveMode
+import org.apache.spark.sql.{ DataFrame, SaveMode }
 import org.apache.spark.sql.hive.HiveContext
 import org.apache.spark.{ SparkConf, SparkContext }
 import org.apache.spark.sql.types.{ StringType, StructField, StructType }
-
 import java.io.File
 
 object ConvertSndData extends App {
@@ -16,10 +15,11 @@ object ConvertSndData extends App {
 
 class Converter {
 
-  val root_folder = "/Users/svend/dev/RIA/snd_v2_1000/actors"
+  //val root_folder = "/Users/svend/dev/RIA/snd_v2_1000/actors"
+  val root_dimension_folder = "/Users/svend/dev/RIA/lab-data-volumes/data-generator/svv/main-volume-1.0.0/lab-data-generator/datagenerator/components/_DB/snd_v2/actors"
   val target_folder = "/Users/svend/dev/RIA/snd_parquet"
 
-  val generationDates = Array( "2016-10-19", "2016-10-20" )
+  val generationDates = List( "2016-10-19", "2016-10-20" )
 
   val sparkConf = new SparkConf()
     .setMaster( "local[*]" )
@@ -29,6 +29,12 @@ class Converter {
 
   val sc: SparkContext = new SparkContext( sparkConf )
   val sqlContext: HiveContext = new HiveContext( sc )
+
+  /**
+   * *****************
+   * utils
+   * *****************
+   */
 
   def registerIdFile( sourceFile: String, actorName: String, idField: String ) = {
 
@@ -49,64 +55,37 @@ class Converter {
     df.registerTempTable( actorName )
   }
 
-  def telcos = {
+  def load_attribute( actorName: String, idField: String, attributeFileName: String, attributeName: String ) = {
 
-    registerIdFile( s"$root_folder/telcos/ids.csv", "telcos", "agent_id" )
+    val sourceFile = s"$root_dimension_folder/$actorName/attributes/$attributeFileName.csv"
+    val tableName = s"${actorName}_${attributeFileName}"
 
-    sqlContext.sql( "select agent_id, " +
-      "\n'distributor' as agent_class," +
-      "\n'some_name' as agent_name," +
-      "\n'some_contact_name' as agent_contact_name," +
-      "\n'some_phone' as gent_contact_phone," +
-      "\n'origin' as distributor_type," +
-      "\n'Floyd Daniels' as distributor_sales_rep_name," +
-      "\n'+44 7531 3579' as distributor_sales_rep_contact_number" +
-      "\nfrom telcos" )
+    val attributeSchema = StructType( Array(
+      StructField( idField, StringType, true ),
+      StructField( attributeName, StringType, true )
+    ) )
+
+    val df =
+      sqlContext
+        .read
+        .format( "com.databricks.spark.csv" )
+        .option( "header", "true" )
+        .schema( attributeSchema )
+        .option( "inferSchema", "false" )
+        .option( "delimiter", "," )
+        .load( sourceFile )
+
+    df.registerTempTable( tableName )
+
+    sqlContext.sql( s"select $idField, $attributeName from $tableName" )
   }
 
-  def dealers =
-
-    List("l1", "l2").map {
-      level => {
-        registerIdFile( s"$root_folder/dealers_$level/ids.csv", "dealers_$level", "agent_id" )
-
-        sqlContext.sql( "select agent_id, " +
-          "\n'distributor' as agent_class," +
-          "\n'some_name' as agent_name," +
-          "\n'some_contact_name' as agent_contact_name," +
-          "\n'some_phone' as gent_contact_phone," +
-          "\n'dealer' as distributor_type," +
-          "\n'Floyd Daniels' as distributor_sales_rep_name," +
-          "\n'+44 7531 3579' as distributor_sales_rep_contact_number" +
-          "\nfrom dealers_l1" )
-      }
-    }.reduce(_ unionAll _ )
-
-
-
-  def pos = {
-
-  }
-
-  def loadDistributors = {
-
-    // TODO: cf Thoralf: load all dealers, telco and POS here ?
-    // should be complete attribute generated in the simulation?
-
-    val all_distributors = telcos.unionAll( dealers )
-
+  def writeDimension( actor: DataFrame, actorName: String ) =
     for ( generationDate <- generationDates ) {
-      val fileName = s"$target_folder/dimensions/distributor/0.1/$generationDate/resource.parquet"
-      all_distributors.write.mode( SaveMode.Overwrite ).parquet( fileName )
+      val fileName = s"$target_folder/dimensions/$actorName/0.1/$generationDate/resource.parquet"
+      actor.write.mode( SaveMode.Overwrite ).parquet( fileName )
     }
 
-    telcos
-  }
-
-  /**
-   *  copy/pasted from
-   * @param file
-   */
   def deleteRecursively( file: File ): Unit = {
     if ( file.isDirectory )
       file.listFiles.foreach( deleteRecursively )
@@ -114,8 +93,61 @@ class Converter {
       throw new Exception( s"Unable to delete ${file.getAbsolutePath}" )
   }
 
+  /**
+   * *****************
+   * circus conversions
+   * *****************
+   */
+
+  def convertPos = {
+
+    val attrs = Map(
+      "AGENT_NAME" -> "agent_name",
+      "CONTACT_NAME" -> "agent_contact_name",
+      "CONTACT_PHONE" -> "agent_contact_phone",
+      "LATITUDE" -> "fixed_pos_latitude",
+      "LONGITUDE" -> "fixed_pos_longitude"
+    )
+
+    // all the generated pos attributes
+    val pos_attrs = attrs.map {
+      case ( fName, attName ) =>
+        load_attribute( actorName = "pos", idField = "agent_id",
+          attributeFileName = fName, attributeName = attName )
+    }.reduce( _.join( _, usingColumn = "agent_id" ) )
+
+    registerIdFile( s"$root_dimension_folder/pos/ids.csv", "pod_ids", "agent_id" )
+
+    // hard-coded values for the rest
+    val pos_fixed = sqlContext.sql( """
+      SELECT agent_id,
+        'pos' AS agent_class,
+        'grocery store' AS pos_type,
+        True AS pos_fixed,
+        'small_retail' AS pos_channel_id,
+        'will be added in OASD-2927' AS fixed_pos_geo_level1_id
+       FROM pod_ids
+    """
+    )
+
+    val pos = pos_fixed.join( pos_attrs, usingColumn = "agent_id" )
+
+    print( "pos: " )
+    pos.printSchema()
+    pos.take( 10 ).foreach( r => println( s" pos row: $r" ) )
+
+    writeDimension( pos, "FixedPos" )
+
+  }
+
+  /**
+   * *****************
+   * main
+   * *****************
+   */
+
   def generate(): Unit = {
-    println( s"converting from $root_folder into $target_folder" )
+    println( s"converting from $root_dimension_folder into $target_folder" )
 
     val target = new File( target_folder )
     if ( target.exists() ) {
@@ -124,9 +156,7 @@ class Converter {
     }
     target.mkdir()
 
-    val telcos = loadDistributors
-    println( telcos.collect() )
-
+    convertPos
   }
 
 }
